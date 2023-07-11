002990f25af1838b67c823d2cfe83d534c8ea833
Nitaki-dev <lucskywalker.berst@gmail.com>
2023-07-11 16:25:34 +0200

    effects update

--- pixel_sorter.js
+++ pixel_sorter.js
@@ -1,8 +1,6 @@
 let img;
 let p5Instance = null;
 
-let visualizeMask = false;
-
 let randomMaskOffset = 15;
 
 let initialLowThreshold;
@@ -54,90 +52,31 @@ function loadImage(event) {
     }
 }
 
-function countUniqueColors(pixels) {
-    let uniqueColors = new Set();
-    
-    for (let i = 0; i < pixels.length; i += 4) {
-        let r = pixels[i];
-        let g = pixels[i+1];
-        let b = pixels[i+2];
-        let colorStr = r + ',' + g + ',' + b;
-        
-        uniqueColors.add(colorStr);
-    }
-    
-    return uniqueColors.size;
-}
-
-function maskFunction(pixel, lowThreshold, highThreshold) {
-    let pixelValue = 0.2989*pixel.r + 0.5870*pixel.g + 0.1140*pixel.b;
-    let normalizedValue = pixelValue / 255;
-    let invertMask = document.getElementById('invertMask').checked;
-
-    if (invertMask) {
-        if (normalizedValue > lowThreshold && normalizedValue < highThreshold) {
-            return false;
-        } else {
-            return true;
-        }
-    } else {
-        if (normalizedValue > lowThreshold && normalizedValue < highThreshold) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
-function maskPixels() {
-    let lowThreshold = parseFloat(document.getElementById('lowThreshold').value);
-    let highThreshold = parseFloat(document.getElementById('highThreshold').value);
-
-    let maskedPixels = [];
-    let mask = [];
-    let r, g, b, a, pixel, masked;
-
-    for (let i = 0; i < p5Instance.pixels.length; i += 4) {
-        r = p5Instance.pixels[i];
-        g = p5Instance.pixels[i + 1];
-        b = p5Instance.pixels[i + 2];
-        a = p5Instance.pixels[i + 3];
-
-        pixel = {r: r, g: g, b: b, a: a};
-
-        masked = maskFunction(pixel, lowThreshold, highThreshold);
-        mask.push(masked);
-
-        if (visualizeMask) {
-            maskedPixels.push(masked ? {r: 255, g: 255, b: 255, a: a} : {r: 0, g: 0, b: 0, a: a});
-        } else {
-            maskedPixels.push(pixel);
-        }
-    }
-
-    return {maskedPixels, mask};
-}
-
-
-function quantizeColor(c, quanta) {return Math.round(c / quanta) * quanta;}
-function applyQuantization(pixel) {pixel.r = quantizeColor(pixel.r, 32); pixel.g = quantizeColor(pixel.g, 32); pixel.b = quantizeColor(pixel.b, 32);}
-function findClosestPaletteColor(oldPixel) {return {r: quantizeColor(oldPixel.r, 32), g: quantizeColor(oldPixel.g, 32), b: quantizeColor(oldPixel.b, 32)};}
-
-function applyDithering(pixels, x, y, quantError) {
-    let index;
-    if (x + 1 < p5Instance.width) {index = 4 * ((y) * p5Instance.width + (x + 1)); pixels[index] += quantError.r * 7/16; pixels[index + 1] += quantError.g * 7/16; pixels[index + 2] += quantError.b * 7/16;}
-    if (x - 1 >= 0 && y + 1 < p5Instance.height) {index = 4 * ((y + 1) * p5Instance.width + (x - 1)); pixels[index] += quantError.r * 3/16; pixels[index + 1] += quantError.g * 3/16; pixels[index + 2] += quantError.b * 3/16;}
-    if (y + 1 < p5Instance.height) {index = 4 * ((y + 1) * p5Instance.width + x); pixels[index] += quantError.r * 5/16; pixels[index + 1] += quantError.g * 5/16; pixels[index + 2] += quantError.b * 5/16;}
-    if (x + 1 < p5Instance.width && y + 1 < p5Instance.height) {index = 4 * ((y + 1) * p5Instance.width + (x + 1)); pixels[index] += quantError.r * 1/16; pixels[index + 1] += quantError.g * 1/16; pixels[index + 2] += quantError.b * 1/16;}
-}
-
 let sortDirection = document.getElementById('sortDirection').value;
 let sortColor = document.getElementById('sortColor').value;
 let reverseSort = document.getElementById('reverseSort').checked;
 let maskSort = document.getElementById('maskSort').checked;
-let selectedEffects = document.getElementById('selectedEffects').value;
-visualizeMask = document.getElementById('displayMask').checked;
+let visualizeMask = document.getElementById('displayMask').checked;
 let ramdomMaskOffset = document.getElementById('ramdomMaskOffset').checked;
+
+let useEffects = document.getElementById('useEffects');
+
+// effects
+let effect_dithering = document.getElementById("effect_dithering");
+let effect_color_quant = document.getElementById("effect_color_quant");
+let effect_posterize = document.getElementById("effect_posterize");
+let effect_pixelate = document.getElementById("effect_pixelate");
+let effect_sharpen = document.getElementById("effect_sharpen");
+let effect_emboss = document.getElementById("effect_emboss");
+let effect_sepia = document.getElementById("effect_sepia");
+
+let effect_invert = document.getElementById("effect_invert");
+let effect_edge_detection = document.getElementById("effect_edge_detection");
+let effect_grayscale = document.getElementById("effect_grayscale");
+let effect_sobel = document.getElementById("effect_sobel");
+let effect_chromatic_aberration = document.getElementById("effect_chromatic_aberration");
+let effect_film_grain = document.getElementById("effect_film_grain");
+let effect_vignette = document.getElementById("effect_vignette");
 
 async function sortPixels() {
     if (img) {
@@ -146,45 +85,27 @@ async function sortPixels() {
         sortColor = document.getElementById('sortColor').value;
         reverseSort = document.getElementById('reverseSort').checked;
         maskSort = document.getElementById('maskSort').checked;
-        selectedEffects = document.getElementById('selectedEffects').value;
         visualizeMask = document.getElementById('displayMask').checked;
         ramdomMaskOffset = document.getElementById('ramdomMaskOffset').checked;
 
-        p5Instance.loadPixels();
+        useEffects = document.getElementById('useEffects');
 
-        if (selectedEffects === 'dithering') {
-            for (let y = 0; y < p5Instance.height; y++) {
-                for (let x = 0; x < p5Instance.width; x++) {
-                    let i = 4 * (y * p5Instance.width + x);
-                    let oldPixel = { r: p5Instance.pixels[i], g: p5Instance.pixels[i + 1], b: p5Instance.pixels[i + 2] };
-                    let newPixel = findClosestPaletteColor(oldPixel);
-    
-                    p5Instance.pixels[i]     = newPixel.r;
-                    p5Instance.pixels[i + 1] = newPixel.g;
-                    p5Instance.pixels[i + 2] = newPixel.b;
-    
-                    let quantError = {
-                        r: oldPixel.r - newPixel.r,
-                        g: oldPixel.g - newPixel.g,
-                        b: oldPixel.b - newPixel.b
-                    };
-                    applyDithering(p5Instance.pixels, x, y, quantError);
-                }
-            }
-        } else if (selectedEffects === 'quantization') {
-            for (let y = 0; y < p5Instance.height; y++) {
-                for (let x = 0; x < p5Instance.width; x++) {
-                    let i = 4 * (y * p5Instance.width + x);
-                    let pixel = { r: p5Instance.pixels[i], g: p5Instance.pixels[i + 1], b: p5Instance.pixels[i + 2] };
-                    
-                    applyQuantization(pixel);
-    
-                    p5Instance.pixels[i] = pixel.r;
-                    p5Instance.pixels[i + 1] = pixel.g;
-                    p5Instance.pixels[i + 2] = pixel.b;
-                }
-            }
-        }
+        effect_dithering = document.getElementById("effect_dithering");
+        effect_color_quant = document.getElementById("effect_color_quant");
+        effect_posterize = document.getElementById("effect_posterize");
+        effect_pixelate = document.getElementById("effect_pixelate");
+        effect_sharpen = document.getElementById("effect_sharpen");
+        effect_emboss = document.getElementById("effect_emboss");
+        effect_sepia = document.getElementById("effect_sepia");
+        effect_invert = document.getElementById("effect_invert");
+        effect_edge_detection = document.getElementById("effect_edge_detection");
+        effect_grayscale = document.getElementById("effect_grayscale");
+        effect_sobel = document.getElementById("effect_sobel");
+        effect_chromatic_aberration = document.getElementById("effect_chromatic_aberration");
+        effect_film_grain = document.getElementById("effect_film_grain");
+        effect_vignette = document.getElementById("effect_vignette");
+
+        p5Instance.loadPixels();
 
         let mask = [];
 
@@ -204,6 +125,22 @@ async function sortPixels() {
                     p5Instance.pixels[index + 3] = pix.a;
                 }
             }
+        }
+
+        if (useEffects.checked) {               
+            if (effect_posterize.checked) effect_posterize_function()
+            if (effect_sharpen.checked) effect_sharpen_function()
+            if (effect_emboss.checked) effect_emboss_function()
+            if (effect_sepia.checked) effect_sepia_function()
+            if (effect_invert.checked) effect_invert_function()
+            if (effect_edge_detection.checked) effect_edge_detection_function()
+            if (effect_grayscale.checked) effect_grayscale_function()
+            if (effect_sobel.checked) effect_sobel_function()
+            if (effect_color_quant.checked) effect_color_quant_function()
+            if (effect_dithering.checked) effect_dithering_function()
+            if (effect_chromatic_aberration.checked) effect_chromatic_aberration_function()
+            if (effect_film_grain.checked) effect_film_grain_function()
+            if (effect_vignette.checked) effect_vignette_function()
         }
 
         let sortFunction;
@@ -326,6 +263,7 @@ async function sortPixels() {
             log("Invalid sorting direction: " + sortDirection);
             return;
         }
+        if (useEffects.checked) if (effect_pixelate.checked) effect_pixelate_function()
 
         p5Instance.updatePixels();
         p5Instance.redraw();
@@ -333,6 +271,79 @@ async function sortPixels() {
     } else {
         log("No image loaded");
     }
+}
+
+function maskFunction(pixel, lowThreshold, highThreshold) {
+    let pixelValue = 0.2989*pixel.r + 0.5870*pixel.g + 0.1140*pixel.b;
+    let normalizedValue = pixelValue / 255;
+    let invertMask = document.getElementById('invertMask').checked;
+
+    if (invertMask) {
+        if (normalizedValue > lowThreshold && normalizedValue < highThreshold) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        if (normalizedValue > lowThreshold && normalizedValue < highThreshold) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+function maskPixels() {
+    let lowThreshold = parseFloat(document.getElementById('lowThreshold').value);
+    let highThreshold = parseFloat(document.getElementById('highThreshold').value);
+
+    let maskedPixels = [];
+    let mask = [];
+    let r, g, b, a, pixel, masked;
+
+    for (let i = 0; i < p5Instance.pixels.length; i += 4) {
+        r = p5Instance.pixels[i];
+        g = p5Instance.pixels[i + 1];
+        b = p5Instance.pixels[i + 2];
+        a = p5Instance.pixels[i + 3];
+
+        pixel = {r: r, g: g, b: b, a: a};
+
+        masked = maskFunction(pixel, lowThreshold, highThreshold);
+        mask.push(masked);
+
+        if (visualizeMask) {
+            maskedPixels.push(masked ? {r: 255, g: 255, b: 255, a: a} : {r: 0, g: 0, b: 0, a: a});
+        } else {
+            maskedPixels.push(pixel);
+        }
+    }
+
+    return {maskedPixels, mask};
+}
+
+function quantizeColor(c, quanta) {
+    return Math.round(c / quanta) * quanta;
+}
+
+function applyQuantization(pixel) {
+    pixel.r = quantizeColor(pixel.r, 32); 
+    pixel.g = quantizeColor(pixel.g, 32); 
+    pixel.b = quantizeColor(pixel.b, 32);
+}
+
+function findClosestPaletteColor(oldPixel) {
+    return {
+        r: quantizeColor(oldPixel.r, 32), g: quantizeColor(oldPixel.g, 32), b: quantizeColor(oldPixel.b, 32)
+    };
+}
+
+function applyDithering(pixels, x, y, quantError) {
+    let index;
+    if (x + 1 < p5Instance.width) {index = 4 * ((y) * p5Instance.width + (x + 1)); pixels[index] += quantError.r * 7/16; pixels[index + 1] += quantError.g * 7/16; pixels[index + 2] += quantError.b * 7/16;}
+    if (x - 1 >= 0 && y + 1 < p5Instance.height) {index = 4 * ((y + 1) * p5Instance.width + (x - 1)); pixels[index] += quantError.r * 3/16; pixels[index + 1] += quantError.g * 3/16; pixels[index + 2] += quantError.b * 3/16;}
+    if (y + 1 < p5Instance.height) {index = 4 * ((y + 1) * p5Instance.width + x); pixels[index] += quantError.r * 5/16; pixels[index + 1] += quantError.g * 5/16; pixels[index + 2] += quantError.b * 5/16;}
+    if (x + 1 < p5Instance.width && y + 1 < p5Instance.height) {index = 4 * ((y + 1) * p5Instance.width + (x + 1)); pixels[index] += quantError.r * 1/16; pixels[index + 1] += quantError.g * 1/16; pixels[index + 2] += quantError.b * 1/16;}
 }
 
 function getSpans(row, maskRow) {
@@ -452,6 +463,26 @@ function setColumn(x, column) {
     }
 }
 
+function updateThresholds(currentFrame, totalFrames) {
+    let lowThreshold = (currentFrame / totalFrames) * finalLowThreshold;
+    let highThreshold = (currentFrame / totalFrames) * finalHighThreshold;
+    return [lowThreshold, highThreshold];
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (document.getElementById('maskSort').checked) {
+        document.getElementById('maskOptions').style.display = 'block';
+    } else {
+        document.getElementById('maskOptions').style.display = 'none';
+    }
+
+    if (document.getElementById('useEffects').checked) {
+        document.getElementById('effect-list').style.display = 'flex';
+    } else {
+        document.getElementById('effect-list').style.display = 'none';
+    }
+});
+
 document.getElementById('maskSort').addEventListener('change', function() {
     if (this.checked) {
         document.getElementById('maskOptions').style.display = 'block';
@@ -460,11 +491,11 @@ document.getElementById('maskSort').addEventListener('change', function() {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    if (document.getElementById('maskSort').checked) {
-        document.getElementById('maskOptions').style.display = 'block';
+document.getElementById('useEffects').addEventListener('change', function() {
+    if (this.checked) {
+        document.getElementById('effect-list').style.display = 'flex';
     } else {
-        document.getElementById('maskOptions').style.display = 'none';
+        document.getElementById('effect-list').style.display = 'none';
     }
 });
 
@@ -475,12 +506,6 @@ document.getElementById('image-export-button').addEventListener('click', functio
         p5Instance.saveCanvas('sorted-image', 'png');
     }
 });
-
-function updateThresholds(frame, totalFrames) {
-    lowThreshold = initialLowThreshold + (finalLowThreshold - initialLowThreshold) * (frame / totalFrames);
-    highThreshold = initialHighThreshold + (finalHighThreshold - initialHighThreshold) * (frame / totalFrames);
-    return [lowThreshold, highThreshold];
-}
 
 document.getElementById('gif-export-button').addEventListener('click', async function() {
     if (!img) {
